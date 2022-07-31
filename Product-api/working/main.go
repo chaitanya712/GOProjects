dbfdbd7a1b647408c2c91a0c4cd40c4828dd6a85
chaitanya712 <71598994+chaitanya712@users.noreply.github.com>
2022-07-31 16:20:38 +0530

    Update main.go

--- Product-api/working/main.go
+++ Product-api/working/main.go
@@ -1,6 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"database/sql"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
 
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
